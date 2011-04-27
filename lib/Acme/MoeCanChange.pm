c84edd7938a33681ba0f517c92a8a200b2930f29
haoyayoi <st.hao.yayoi@gmail.com>
2011-04-27 23:08:18 +0900

    initial commit

---
+++ lib/Acme/MoeCanChange.pm
@@ -0,0 +1,33 @@
+package Acme::MoeCanChange;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::MoeCanChange -
+
+=head1 SYNOPSIS
+
+  use Acme::MoeCanChange;
+
+=head1 DESCRIPTION
+
+Acme::MoeCanChange is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
